c91f5e2b628aa0b26945a556436ad06cd5059967
Maurice Prosper <maurice.prosper@ttu.edu>
2015-08-12 01:30:58 -0500

    Update on any inputs value changing, more readability

--- js/rok/update.js
+++ js/rok/update.js
@@ -1,23 +1,34 @@
 /**
  * data has changed
  */ 
-$('input').on('keyup keypress blur change', function () {
+$('body').on('keyup keypress blur change', 'input', function () {
 	var cost = 0, left;
 
-	// get the cost
-	$('#list input').each(function() {
-		var price = grab(
-				$(this).attr('data-item'),
+	// get the cost of each material
+	$('.materials input').each(function() {
+		var item = $(this).attr('data-item'),
+			price = grab(
+				item,
 				$(this).val(),
 				$('#build_per').val() / 100 + 1,
 				materials
-			);
+			),
+			bell = $('span[data-item="'+ item +'"]');
 
-		$(this).next('.spent').text( numeral(Math.ceil(price)).format('0,0') );
-
+		// value spent of certain material
+		if($(this).val())
+			bell
+				.removeClass('hide')
+				.text( numeral(Math.ceil(price)).format('0,0') );
+		else
+			bell
+				.addClass('hide');
+		
 		cost += price;
 	});
 
+	// total value left
 	left = $('#cash').val() - cost;
+	
 	$('#left').text( numeral(Math.ceil(left)).format('0,0') );
 });
